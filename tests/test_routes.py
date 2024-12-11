30d9a8207baa8af6cc5b889b1275e28753720b26
Vladimir Alfaro <vladimir.ac29@gmail.com>
2024-12-11 12:08:54 -0400

    chore: fix test for routes

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,5 +1,9 @@
 import unittest
 from app import create_app
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 class RoutesTestCase(unittest.TestCase):
     def setUp(self):
@@ -8,30 +12,30 @@ class RoutesTestCase(unittest.TestCase):
         self.app.testing = True
 
     def test_create_note(self):
-        response = self.client.post('/notes', json={'title': 'Test Note', 'content': 'This is a test note.'})
+        response = self.client.post(f'/api/{os.getenv('API_VERSION')}/notes', json={'title': 'Test Note', 'content': 'This is a test note.'})
         self.assertEqual(response.status_code, 201)
         self.assertIn('id', response.get_json())
 
     def test_get_notes(self):
-        response = self.client.get('/notes')
+        response = self.client.get(f'/api/{os.getenv('API_VERSION')}/notes')
         self.assertEqual(response.status_code, 200)
 
     def test_get_note_by_id(self):
-        response = self.client.post('/notes', json={'title': 'Test Note', 'content': 'This is a test note.'})
+        response = self.client.post(f'/api/{os.getenv('API_VERSION')}/notes', json={'title': 'Test Note', 'content': 'This is a test note.'})
         note_id = response.get_json()['id']
-        response = self.client.get(f'/notes/{note_id}')
+        response = self.client.get(f'/api/{os.getenv('API_VERSION')}/notes/{note_id}')
         self.assertEqual(response.status_code, 200)
 
     def test_update_note(self):
-        response = self.client.post('/notes', json={'title': 'Test Note', 'content': 'This is a test note.'})
+        response = self.client.post(f'/api/{os.getenv('API_VERSION')}/notes', json={'title': 'Test Note', 'content': 'This is a test note.'})
         note_id = response.get_json()['id']
-        response = self.client.put(f'/notes/{note_id}', json={'title': 'Updated Note', 'content': 'Updated content.'})
+        response = self.client.put(f'/api/{os.getenv('API_VERSION')}/notes/{note_id}', json={'title': 'Updated Note', 'content': 'Updated content.'})
         self.assertEqual(response.status_code, 200)
 
     def test_delete_note(self):
-        response = self.client.post('/notes', json={'title': 'Test Note', 'content': 'This is a test note.'})
+        response = self.client.post(f'/api/{os.getenv('API_VERSION')}/notes', json={'title': 'Test Note', 'content': 'This is a test note.'})
         note_id = response.get_json()['id']
-        response = self.client.delete(f'/notes/{note_id}')
+        response = self.client.delete(f'/api/{os.getenv('API_VERSION')}/notes/{note_id}')
         self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
